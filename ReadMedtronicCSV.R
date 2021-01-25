be7898c628c7b9ecc9816af1a1185e41766867b1
dr R <raimun2@users.noreply.github.com>
2021-01-25 15:05:58 -0300

    Create ReadMedtronicCSV.R

---
+++ ReadMedtronicCSV.R
@@ -0,0 +1,25 @@
+#readcsv medtronic
+read_medtronic_csv <- function(archivo){
+  require(janitor)
+  archivo <- readLines(archivo)
+  
+  blancas <- which(archivo == "")
+  
+  nomcols <- read.table(text=archivo[1], header=FALSE, sep=",")
+  subject <- read.table(text=archivo[2], header=FALSE, sep=",")
+  colnames(subject) <- nomcols[1:ncol(subject)]
+  
+  bomba1 <- archivo[(blancas[2]+2):(blancas[3]-1)]
+  bomba2 <- archivo[(blancas[3]+2):(blancas[4]-1)]
+  
+  bomba1 <- read.table(text=bomba1, header=TRUE, sep=",")
+  bomba1 <- bomba1 %>%  remove_empty("cols")
+  
+  bomba2 <- read.table(text=bomba2, header=TRUE, sep=",")
+  bomba2 <- bomba2 %>%  remove_empty("cols")
+  
+  return(list("subject" = subject, 
+              "transactions" = bomba1, 
+              "readings" = bomba2))
+}
+
